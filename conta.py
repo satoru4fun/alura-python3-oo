68b3dbe00a6325a9fa81f2c1925e0886e7f1983a
Guilherme Noguchi <8411808@educacao.intranet>
2019-05-13 09:02:26 -0300

    aula 5

--- conta.py
+++ conta.py
@@ -2,16 +2,29 @@
 
 class Conta():
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
     
     def extrato(self):
-        print('Saldo de {} do titular {}'.format(self.saldo, self.titular))
+        print('Saldo de {} do titular {}'.format(self.__saldo, self.__titular))
     
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
     
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+    
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+    
+    def get_titular(self):
+        return self.__titular
+    
+    def get_limite(self):
+        return self.__limite
+    
+    def set_limite(self, limite):
+        self.__limite = limite
